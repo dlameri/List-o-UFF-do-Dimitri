b6efc7a06ef5395156c48c63e4c6383c8d4f8c22
Dimitri Lameri <dimitrilameri@gmail.com>
2011-12-28 03:19:59 -0200

    Adicionados os dados da primeira fase

--- app/admin/aluno.rb
+++ app/admin/aluno.rb
@@ -7,7 +7,7 @@ ActiveAdmin.register Aluno do
 	    	@year = params[:aluno][:ano_id]
 	    	@fileParam1fase = params[:aluno][:txtFile1fase]
 	    	@fileParam1fase = params[:aluno][:txtFile2fase]
-    		unless @fileParam1fase.nil?
+    		if ! @fileParam1fase.nil? then
       			if Aluno.process_file_primeira_fase(@fileParam1fase, @year)
 					flash[:notice] = "Arquivo incluido com sucesso !"
       				redirect_to :action => :index
@@ -15,7 +15,7 @@ ActiveAdmin.register Aluno do
       				flash[:alert] = "Ocorreu um erro ao tentar incluir o arquivo"
       				redirect_to :action => :new
 				end
-			else unless @fileParam2fase.nil?
+			elsif ! @fileParam2fase.nil? then
 				if Aluno.process_file_segunda_fase(@fileParam2fase, @year)
 					flash[:notice] = "Arquivo incluido com sucesso !"
       				redirect_to :action => :index
